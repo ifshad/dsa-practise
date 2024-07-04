d3f385e0868dc4ed5817f64aa7168df6c3e18e02
ifshad <shoronshark@gmail.com>
2024-07-04 16:33:08 +0600

    day 3 practise done

--- day_3_hash_table/practise.js
+++ day_3_hash_table/practise.js
@@ -30,3 +30,29 @@ function findIndicesSum(array, targetValue) {
 }
 
 // console.log(findIndicesSum(nums, target))
+
+// Q2. Given two strings s and t, determine if they are isomorphic. Two strings s and t are isomorphic is the characters in s can be replaced to get t. All occurances of the characters must be replaces with another character while preserving the order of characters. No two characters can map to the same character, but a character may map to itself. s and t consist of any valid ASCII character.
+
+function findIsomorphic(s, t) {
+    const sHash = {};
+    const tHash = {};
+    if (s.length !== t.length) {
+        return false;
+    };
+    for (let i = 0; i < s.length; i++) {
+        let charS = s[i];
+        let charT = t[i];
+        if (!sHash[charS]) {
+            sHash[charS] = charT;
+        }
+        if (!tHash[charT]) {
+            tHash[charT] = charS;
+        }
+        if (sHash[charS] !== charT || tHash[charT] !== charS) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// console.log(findIsomorphic('baba' , 'pipe'))
